321bafc84d0b914388331e5690edcd774f8e5b1f
Saulo <saulobenicio2016@gmail.com>
2026-09-02 23:57:41 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           {children}
           <Toaster richColors position="top-right" />
         </ThemeProvider>
-        <Analytics />
+        {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
     </html>
   )
